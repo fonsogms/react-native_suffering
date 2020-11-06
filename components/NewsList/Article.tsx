aeae338b217e70bdb95b1bd25dfaa86c1316129e
Alfonso <fonsogms@gmail.com>
2020-11-06 17:08:49 +0100

    styling ios better

--- components/NewsList/Article.tsx
+++ components/NewsList/Article.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Text, View, Image, TouchableOpacity } from "react-native";
 import { Article } from "./news.interface";
-import something from "@react-navigation/native";
 const NewInfo = (props: { article: Article; navigation: any }) => {
   return (
     <View
       style={{
         margin: 10,
+        backgroundColor: "#fff",
+        borderRadius: 10,
         shadowColor: "#000",
         shadowOffset: {
           width: 0,
           height: 1,
         },
         shadowOpacity: 0.22,
-        shadowRadius: 10,
+        shadowRadius: 2.22,
 
         elevation: 3,
       }}
@@ -31,7 +32,6 @@ const NewInfo = (props: { article: Article; navigation: any }) => {
       >
         <Image
           style={{
-            flex: 1,
             marginTop: 10,
             width: "100%",
             height: 100,
